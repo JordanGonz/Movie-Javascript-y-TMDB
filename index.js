44ac891520e4bec1534826bc43774e35b0062b69
JordanGonz <jordan.grex23ok@gmail.com>
2023-07-18 22:29:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function createMovieCard(movie) {
         <div class="column">
             <div class="card">
                 <a class="card-media" href="./img-01.jpeg">
-                    <img src="${imagePath}" alt="${original_title}" width="100%" />
+                    <img src="${imagePath}" alt="${original_title}" width="100%" fsdfsfsdfsdf/>
                 </a>
                 <div class="card-content">
                     <div class="card-header">
